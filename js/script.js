2024ca32c17bbddd327ddd1b68fe71b8fe9a86db
Arpit Pandey <156664708+ArpitPandey01@users.noreply.github.com>
2024-02-08 09:39:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -92,7 +92,7 @@ async function displayAlbums() {
   let array = Array.from(anchors);
   for (let index = 0; index < array.length; index++) {
     const e = array[index];
-    if (e.href.includes("/songs/") && !e.href.includes(".htaccess")) {
+    if (e.href.includes("/songs/"){
       let folder = e.href.split("https://arpitpandey01.github.io/SpotifyClone/").slice(-2)[0];
       //Get The meta deta of the folder
       let a = await fetch(`/songs/${folder}/info.json`);
